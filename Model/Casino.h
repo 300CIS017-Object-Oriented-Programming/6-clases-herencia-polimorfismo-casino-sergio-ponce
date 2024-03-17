b27d5662898662dec97de44d885bf3693ed1bec5
Sergio Ponce <1105366347@javerianacali.edu.co>
2024-03-17 15:18:06 -0500

    Prueba para ver si funciona el push

---
+++ Model/Casino.h
@@ -0,0 +1,14 @@
+//
+// Created by sergio on 17/03/24.
+//
+
+#ifndef CASINO_POLIMORFISMO_CASINO_H
+#define CASINO_POLIMORFISMO_CASINO_H
+
+
+class Casino {
+
+};
+
+
+#endif //CASINO_POLIMORFISMO_CASINO_H
